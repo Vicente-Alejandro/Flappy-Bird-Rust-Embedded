e094d9c05849d7c325c4d17a8aed6e674324b198
Vicente Alejandro <realumanin@gmail.com>
2026-07-22 22:28:08 -0400

    refactor(core): move save.ron to data directory
    - Changed SAVE_PATH to data/save.ron to keep root directory clean
    - Added automatic parent directory creation during save
    - Updated .gitignore to exclude the new data/ directory

--- src/core/save.rs
+++ src/core/save.rs
@@ -7,7 +7,7 @@ pub struct SaveData {
     pub high_score: u32,
 }
 
-const SAVE_PATH: &str = "save.ron";
+const SAVE_PATH: &str = "data/save.ron";
 
 pub fn load_save_data(mut commands: Commands) {
     let save_data = if let Ok(data) = fs::read_to_string(SAVE_PATH) {
@@ -20,10 +20,10 @@ pub fn load_save_data(mut commands: Commands) {
 
 pub fn save_save_data(save_data: Res<SaveData>) {
     if save_data.is_changed() {
-        let Ok(data) = ron::ser::to_string_pretty(&*save_data, ron::ser::PrettyConfig::default())
-        else {
-            return;
-        };
+        let Ok(data) = ron::ser::to_string_pretty(&*save_data, ron::ser::PrettyConfig::default()) else { return; };
+        if let Some(parent) = std::path::Path::new(SAVE_PATH).parent() {
+            let _ = fs::create_dir_all(parent);
+        }
         let _ = fs::write(SAVE_PATH, data);
     }
 }
